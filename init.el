e75edbfdea4958b206b03be886b40b19fa1ce0d9
Jorge Gomez <jorgemario@belucasys.com>
2017-07-21 09:02:54 -0500

    narrow-wide dwin

--- init.el
+++ init.el
@@ -210,6 +210,44 @@
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
+;; Endless functionality
+
+(defun narrow-or-widen-dwim (p)
+  "Widen if buffer is narrowed, narrow-dwim otherwise.
+Dwim means: region, org-src-block, org-subtree, or
+defun, whichever applies first. Narrowing to
+org-src-block actually calls `org-edit-src-code'.
+
+With prefix P, don't widen, just narrow even if buffer
+is already narrowed."
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen) (recenter))
+        ((region-active-p)
+         (narrow-to-region (region-beginning)
+                           (region-end)))
+        ((derived-mode-p 'org-mode)
+         ;; `org-edit-src-code' is not a real narrowing
+         ;; command. Remove this first conditional if
+         ;; you don't want it.
+         (cond ((ignore-errors (org-edit-src-code) t)
+                (delete-other-windows))
+               ((ignore-errors (org-narrow-to-block) t))
+               (t (org-narrow-to-subtree))))
+        ((derived-mode-p 'latex-mode)
+         (LaTeX-narrow-to-environment))
+        (t (narrow-to-defun))))
+
+;; This line actually replaces Emacs' entire narrowing
+;; keymap, that's how much I like this command. Only
+;; copy it if that's what you want.
+(define-key ctl-x-map "n" #'narrow-or-widen-dwim)
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (define-key LaTeX-mode-map "\C-xn"
+              nil)))
+
+
 (require 'use-package)
 (setq use-package-verbose t)
 
@@ -443,6 +481,7 @@ Start `ielm' if it's not already running."
 ;; See https://github.com/bbatsov/prelude/blob/master/modules/prelude-helm.el
 (use-package helm
   :ensure t
+  :diminish helm-mode
   :init (progn
           (require 'helm-config)
           (require 'helm-eshell)
