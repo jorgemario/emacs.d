f52726d60e7c05ef1b4937b1f8c3fc04e93bd059
Jorge Gomez <jorgemario@belucasys.com>
2017-07-26 10:57:00 -0500

    fix line number scaling

--- modules/simo-ui.el
+++ modules/simo-ui.el
@@ -5,15 +5,25 @@
 ;;; Code:
 
 (require 'use-package)
+(require 'linum)
 
 ;; trust all themes
 (setq custom-safe-themes t)
 
 (set-frame-font "Hack 14" nil t)
 
+;; fix line number scaling
 ;; see https://unix.stackexchange.com/questions/29786/font-size-issues-with-emacs-in-linum-mode/30087#30087
-;; (eval-after-load "linum"
-;;   '(set-face-attribute 'linum nil :height 120))
+(defun linum-update-window-scale-fix (win)
+  "fix linum for scaled text"
+  (set-window-margins win
+          (ceiling (* (if (boundp 'text-scale-mode-step)
+                  (expt text-scale-mode-step
+                    text-scale-mode-amount) 1)
+              (if (car (window-margins))
+                  (car (window-margins)) 1)
+              ))))
+(advice-add #'linum-update-window :after #'linum-update-window-scale-fix)
 
 (use-package gruvbox-theme
   :ensure t
